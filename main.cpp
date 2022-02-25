e1f7ceb9d45e8fbd53199c5cd8640680dcc59b40
ryancalizadeh <ryancalizadeh@gmail.com>
2022-02-25 18:44:34 -0500

    install nanovg

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 #include "Vector3.h"
 
+#ifdef NANOVG_GLEW
+#  include <GL/glew.h>
+#endif
+#define GLFW_INCLUDE_GLEXT
+#include <GLFW/glfw3.h>
+#include "nanovg-master/src/nanovg.h"
+#define NANOVG_GL2_IMPLEMENTATION
+#include "nanovg-master/src/nanovg_gl.h"
+#include "nanovg-master/example/demo.h"
+
 int main() {
     Vector3 *a = new Vector3(1.0, 2.0, 3.7);
     Vector3 *b = new Vector3(2.0, -0.2, 8.0);
